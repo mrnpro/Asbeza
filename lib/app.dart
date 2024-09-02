427b536265f6425dbede684b96c0603048dbf3c8
Natnael Fikadu <88460514+mrnpro@users.noreply.github.com>
2024-09-03 00:24:53 +0300

    feat: lock portrait

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'core/config/index.dart';
 import 'screens/screens.dart';
@@ -7,6 +8,11 @@ class App extends HookConsumerWidget {
   const App({super.key});
   @override
   Widget build(BuildContext context, ref) {
+    // lock portrait
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     final theme = ref.watch(themeProviderProvider);
     return MaterialApp(
         title: 'Asbeza APP', theme: theme, home: const EntryScreen());
